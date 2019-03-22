68ea616126b2e4075210b88155d85423736e5ae8
Tristan Tyler <fehnware@outlook.com>
2019-03-22 14:19:18 +0000

    New methods and properties
    Added new features:
    New properties and Method for handling a products Price values.
    New properties and Method for handling a products Category information.

--- Product.php
+++ Product.php
@@ -12,24 +12,28 @@
 */
 
 class Product {
-	
-	public $loadSuccessful;
+
+	public $detailsValid;
+	public $priceValid;
+	public $categoryValid;
 	public $error;
 	public $details;
+	public $price;
+	public $category;
 	protected $files;
 	protected $dbAttributes;
 	protected static $dbConn;
-	
+
 	//Constructor method.
-	
+
 	public function __construct($searchParam, $dbAttributes, $dbConn) {
 		$this->set_DB_Attributes($dbAttributes);
 		self::$dbConn = $dbConn;
 		$this->set_details($searchParam);
 	}
-	
+
 	//Constructor optional input variables initialisation.
-	
+
 	protected function set_DB_Attributes($dbAttributes) {
 		if(isset($dbAttributes) && is_array($dbAttributes)) {
 			$this->dbAttributes = $dbAttributes;
@@ -38,28 +42,28 @@ class Product {
 			$this->dbAttributes = array("TableName"=>"Products", "UniqueIdentifierField"=>"id", "FieldsToReturn"=>"*");
 		}
 	}
-	
-	//protected methods. 
-	
+
+	//protected methods.
+
 	protected function set_details($searchParam) {
 		$searchParam = $this->check_params($searchParam);
 		$query = "SELECT * From ".$this->dbAttributes["TableName"]." where ".$this->construct_where_string($searchParam);
 		$this->details = $this->run_query($query, $searchParam);
-		if(count($this->details) == 0) { $this->loadSuccessful = false; }
-		else { $this->loadSuccessful = true; }
+		if(count($this->details) == 0) { $this->detailsValid = false; }
+		else { $this->detailsValid = true; }
 	}
-	
+
 	protected function check_result($query, $error) {
 				$this->error[]['Query'] = $query;
 				$this->error[]['Error'] = $error;
 	}
-	
+
 	protected function run_query($query, $searchParam) {
 		$paramList = $this->construct_param_list($searchParam);
 		$paramTypes = $this->get_param_types($searchParam);
 		return $this->get_database_result($query, $paramTypes, $paramList);
 	}
-	
+
 	protected function construct_param_list($params) {
 		$return = array();
 		for($c = 0; $c < count($params); $c++) {
@@ -67,7 +71,7 @@ class Product {
 		}
 		return $return;
 	}
-	
+
 	protected function construct_where_string($params) {
 		$whereArr = array();
 		for($c = 0; $c < count($params); $c++) {
@@ -75,7 +79,7 @@ class Product {
 		}
 		return implode("AND", $whereArr);
 	}
-	
+
 	protected function check_params($array) {
 		reset($array);
 		if(is_string(key($array))) {
@@ -86,7 +90,7 @@ class Product {
 			return $array;
 		}
 	}
-	
+
 	protected function get_param_types($params) {
 		$paramString = "";
 		for($c = 0; $c < count($params); $c++) {
@@ -94,7 +98,7 @@ class Product {
 		}
 		return $paramString;
 	}
-	
+
 	protected function get_database_result($query, $paramTypes, $paramList) {
 		$result = array();
 		if($stmt = self::$dbConn->prepare($query)) {
@@ -125,7 +129,7 @@ class Product {
 		else { $this->check_result($query, self::$dbConn->error); }
 		return $result;
 	}
-	
+
 	protected function bind_param_array($paramTypes, &$paramList, $stmt) {
 		$params[0] = $paramTypes;
 		for($c = 1; $c <= count($paramList); $c++) {
@@ -133,16 +137,31 @@ class Product {
 		}
 		call_user_func_array(array($stmt, "bind_param"), $params);
 	}
-	
+
 	protected function bind_result_array(&$fields, $stmt) {
 		$meta = $stmt->result_metadata();
-		while ($field = $meta->fetch_field()) { 
-			$params[] = &$fields[$field->name]; 
-		} 
+		while ($field = $meta->fetch_field()) {
+			$params[] = &$fields[$field->name];
+		}
 		call_user_func_array(array($stmt, "bind_result"), $params);
 	}
-	
+
 	//Public Methods
+
+	protected function set_price($query, $searchParam) {
+		$searchParam = $this->check_params($searchParam);
+		$this->price = $this->run_query($query, $searchParam);
+		if(count($this->price) == 0) { $this->priceValid = false; }
+		else { $this->priceValid = true; }
+	}
+
+	protected function set_category($query, $searchParam) {
+		$searchParam = $this->check_params($searchParam);
+		$this->category = $this->run_query($query, $searchParam);
+		if(count($this->category) == 0) { $this->categoryValid = false; }
+		else { $this->categoryValid = true; }
+	}
+
 }
 
 ?>
